c506efe4f64c34be1baf82a9d748d84b6c789829
200153 <200153@e0601-05.JC-21.local>
2022-01-31 11:33:23 +0900

    クラスコードを入れた

--- src/OtoSample02.java
+++ src/OtoSample02.java
@@ -1,15 +1,15 @@
 import jp.jc21.t.yoshizawa.otopika.Otopika;
 
 /**
- *
+ * 
  */
 
 
 /**
- * @author AB2B07
+ * @author t.yoshizawa
  *
  */
-public class OtoSample01  {
+public class OtoSample02  {
 
 	/**
 	 * @param args
@@ -19,15 +19,11 @@ public class OtoSample01  {
 		oto.powerOn();
 		while(true) {
 			oto.lightAll();
-			oto.sleep();
 			oto.lightOff();
 			oto.sleep();
 			oto.lightGreen();
-			oto.sleep();
 			oto.lightGreenYellow();
-			oto.sleep();
 			oto.lightYellow();
-			oto.sleep();
 			oto.lightYellowRed();
 			oto.sleep();
 			oto.lightRed();
